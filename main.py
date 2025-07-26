feeb0431c65c3b7164c9759e5cd86fcaf486329b
Charley1225 <48221740+Charley1225@users.noreply.github.com>
2025-07-27 06:40:18 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from sonnet_chat import ask_sonnet
 import json
+import schedule
 
 intents = discord.Intents.default()
 intents.message_content = True
